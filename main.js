c4b516aab585257198b4031ca3c90e21441366ec
Artem27-93 <artem27negoda@gmail.com>
2020-07-06 01:12:17 +0300

    change navbar

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-var inputNumPage = document.querySelector('#page-num');
-var btnPrev = document.querySelector('#btn-prev');
-var btnNext = document.querySelector('#btn-next');
-var limitPage = document.querySelector('#limit-page');
-var imagesBlock = document.querySelector('#images-block');
+const inputNumPage = document.querySelector('#page-num');
+const btnPrev = document.querySelector('#btn-prev');
+const btnNext = document.querySelector('#btn-next');
+const limitPage = document.querySelector('#limit-page');
+const imagesBlock = document.querySelector('#images-block');
 
 
 function sendQuery(pageNum, limit) {
     imagesBlock.className = 'loading';
 
-    var promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve, reject) => {
         // 1. Создаём новый объект XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-        var client_id = 'ca5a2a324ba06f2cf8bede88a989bb6c2f5f87730032b3c6256b72888f2cc94c';
-        var page = pageNum;
-        var per_page = limit;
-        var url = `https://api.unsplash.com/photos/?page=${page}&per_page=${per_page}&client_id=${client_id}`;
+        const xhr = new XMLHttpRequest();
+        const client_id = 'ca5a2a324ba06f2cf8bede88a989bb6c2f5f87730032b3c6256b72888f2cc94c';
+        const page = pageNum;
+        const per_page = limit;
+        let url = `https://api.unsplash.com/photos/?page=${page}&per_page=${per_page}&client_id=${client_id}`;
 
         // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
         xhr.open('GET', url, true);
@@ -27,39 +27,35 @@ function sendQuery(pageNum, limit) {
             if (this.status === 200) {
                 resolve(this.responseText);
             } else {
-                reject('err' + this.status);
+                reject('err' + ' ' +  this.status);
+                
             }
-
         }
     })
-
-    var renderImages = function (dataHtml) {
-        var html = dataHtml.join('');
+    
+    let jsonFormat = dataJSON => JSON.parse(dataJSON); 
+    let htmlFormat = data => data.map(image => `<img src="${image.urls.small}" />`);
+    let renderImages = function (dataHtml) {
+        let html = dataHtml.join('');
         imagesBlock.innerHTML = html;
+    } 
 
-    }
-    var htmlFormat = data => data.map(image => `<img src="${image.urls.small}" />`);
-
-
-    var jsonFormat = dataJSON => JSON.parse(dataJSON);
 
 
     promise
-        .then(jsonFormat)
-        .then(htmlFormat)
-        .then(renderImages)
+        .then(jsonFormat)  // из JSON в объект JS
+        .then(htmlFormat)  // достаём картинку из объекта
+        .then(renderImages) // рендерим на экран картинки
         .then(() => {
             setTimeout(() => {
             imagesBlock.className = ''
-            }, 800);
-        });
+            }, 600);
+        }); // через 600мс после перерендеринга убираем класс 'Loading'
 }
 
 sendQuery(1, 6);
 
 function changePrev() {
-
-    console.log(limitPage.value);
     inputNumPage.value > 1
         ? inputNumPage.value--
         : inputNumPage.value = 1;
@@ -69,7 +65,7 @@ function changePrev() {
 function changeNext() {
     inputNumPage.value ++;
     
-        sendQuery(inputNumPage.value, limitPage.value) ;
+    sendQuery(inputNumPage.value, limitPage.value) ;
 }
 function changeLimit() {
     sendQuery(inputNumPage.value, limitPage.value) ;
